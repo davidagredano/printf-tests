e35d39ee75102f8a6ee936a2c461567d501e8a71
David Agredano Granell <dagredan@student.42barcelona.com>
2025-01-08 16:37:14 +0100

    Add basic tests for unequal args and directives

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 12:00:02 by dagredan          #+#    #+#             */
-/*   Updated: 2025/01/08 12:29:04 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/01/08 16:17:32 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 void	ft_printf_tests(void)
 {
-	printf("test\n");
+	ft_printf("Same args than directives % %\n", "hello", "bye");
+	ft_printf("More args than directives %\n", "1", "2");
+	//ft_printf("More directives than args % %\n", "1"); // May segfault
 	return ;
 }
 

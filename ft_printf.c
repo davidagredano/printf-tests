85a4ff204aa25d56413ae27ddee94954014d850f
David Agredano Granell <dagredan@student.42barcelona.com>
2025-01-09 18:55:36 +0100

    Add some integration tests

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 12:00:02 by dagredan          #+#    #+#             */
-/*   Updated: 2025/01/08 16:17:32 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/01/09 18:35:55 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,17 @@
 
 void	ft_printf_tests(void)
 {
-	ft_printf("Same args than directives % %\n", "hello", "bye");
-	ft_printf("More args than directives %\n", "1", "2");
+	/* Testing directives - args proportion */
+	ft_printf("Same args than directives %s %s\n", "hello", "bye");
+	ft_printf("More args than directives %s\n", "1", "2");
 	//ft_printf("More directives than args % %\n", "1"); // May segfault
-	return ;
+
+	/* Testing formats */
+	ft_printf("My name is %s and I'm %d y.o.\n", "Alex", 15);
+	ft_printf("%X and %x are equivalent to %i.\n", 123, 123, 123);
+	char	*str = "The address of this string is %p.\n";
+	ft_printf(str, (void *) str);
+	
 }
 
 int	main(void)

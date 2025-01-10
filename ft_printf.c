96388468e7fdf90d92a00a4966626854254fdf60
David Agredano Granell <dagredan@student.42barcelona.com>
2025-01-10 11:00:08 +0100

    Update ft_printf tests to check the return values

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 12:00:02 by dagredan          #+#    #+#             */
-/*   Updated: 2025/01/09 18:35:55 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/01/10 10:53:55 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,35 @@
 
 void	ft_printf_tests(void)
 {
-	/* Testing directives - args proportion */
-	ft_printf("Same args than directives %s %s\n", "hello", "bye");
-	ft_printf("More args than directives %s\n", "1", "2");
-	//ft_printf("More directives than args % %\n", "1"); // May segfault
+	int	ret;
 
 	/* Testing formats */
-	ft_printf("My name is %s and I'm %d y.o.\n", "Alex", 15);
-	ft_printf("%X and %x are equivalent to %i.\n", 123, 123, 123);
-	char	*str = "The address of this string is %p.\n";
-	ft_printf(str, (void *) str);
+	printf("\nTesting formats");
+	printf("\n***************\n");
+	ret = ft_printf("Hello %s!", "World");
+	printf(" | ret=%d\n", ret);
+	ret = ft_printf("My name is %s and I'm %d y.o.", "Alex", 15);
+	printf(" | ret=%d\n", ret);
+	ret = ft_printf("%X and %x are equivalent to %i.", 123, 123, 123);
+	printf(" | ret=%d\n", ret);
+	ret = ft_printf("uint %u don't fit in int: %i.", 3000000000, 3000000000);
+	printf(" | ret=%d\n", ret);
+	ret = ft_printf("%s's first leter is %c.", "David", 'D');
+	printf(" | ret=%d\n", ret);
+	char	*str = "The address of this string is %p.";
+	ret = ft_printf(str, (void *) str);
+	printf(" | ret=%d\n", ret);
+	ret = ft_printf("The %% sign is printed.");
+	printf(" | ret=%d\n", ret);
 	
+	/* Testing directives - args proportion */
+	printf("\nTesting directives - args proportion");
+	printf("\n************************************\n");
+	ret = ft_printf("Same args than directives %s, %s.", "hello", "bye");
+	printf(" | ret=%d\n", ret);
+	ret = ft_printf("More args than directives %s.", "1", "2");
+	printf(" | ret=%d\n", ret);
+	//ft_printf("More directives than args %s %s\n", "1"); // May segfault
 }
 
 int	main(void)

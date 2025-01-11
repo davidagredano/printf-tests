754083901edccc69111037453b25c9575571675c
David Agredano <davidagredano@gmail.com>
2025-01-11 15:41:06 +0100

    Add utility functions for testing

--- printf_tests.h
+++ printf_tests.h
@@ -6,15 +6,24 @@
 /*   By: dagredan <dagredan@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 12:02:19 by dagredan          #+#    #+#             */
-/*   Updated: 2025/01/09 10:41:49 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/01/11 15:10:54 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PRINTF_TESTS_H
 # define PRINTF_TESTS_H
 
+# define CYAN "\e[36m"
+# define GRAY "\e[1;30m"
+# define COLOR_RESET "\e[0m"
+
 # include "../printf/ft_printf.h"
 
 # include <limits.h>
+# include <stdio.h>
+# include <string.h>
+
+void	ft_print_test_header(const char *str);
+void	ft_print_test_desc(const char *str);
 
 #endif
---
+++ utils.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dagredan <dagredan@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/11 15:07:26 by dagredan          #+#    #+#             */
+/*   Updated: 2025/01/11 15:22:34 by dagredan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "printf_tests.h"
+
+void	ft_print_test_header(const char *str)
+{
+	size_t	size;
+	size_t	i;
+
+	size = strlen(str);
+	printf("%s", CYAN);
+	i = 0;
+	while (i++ < size)
+		printf("*");
+	printf("\n%s\n", str);
+	i = 0;
+	while (i++ < size)
+		printf("*");
+	printf("%s\n", COLOR_RESET);
+}
+
+void	ft_print_test_desc(const char *str)
+{
+	printf("\n%s%s%s\n", CYAN, str, COLOR_RESET);
+}

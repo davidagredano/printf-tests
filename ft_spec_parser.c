68bb3efbdb80e14bb174e5cef7126da1e517c72e
David Agredano <davidagredano@gmail.com>
2025-01-18 17:05:01 +0100

    Restructure file and add test cases for specification isvalid and parse tests

--- ft_spec_parser.c
+++ ft_spec_parser.c
@@ -6,27 +6,15 @@
 /*   By: dagredan <dagredan@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 07:43:59 by dagredan          #+#    #+#             */
-/*   Updated: 2025/01/14 09:31:47 by dagredan         ###   ########.fr       */
+/*   Updated: 2025/01/18 16:58:36 by dagredan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tests.h"
 
-static void	ft_isvalid_test(const char *specification)
-{
-	char *complete_spec;
-
-	complete_spec = ft_strjoin("%", specification);
-	ft_print_test_desc(specification);
-	printf(complete_spec, "Valid");
-	printf(" %s// printf%s\n", GRAY, COLOR_RESET);
-	if (ft_isvalid(specification))
-		printf("Valid");
-	else
-		printf("Not valid");
-	printf(" %s// isvalid%s\n", GRAY, COLOR_RESET);
-	free(complete_spec);
-}
+static void	ft_isvalid_test(const char *specification);
+static void	ft_parse_test(const char *specification, t_spec *spec);
+static void	ft_log_spec(t_spec *spec);
 
 void	ft_isvalid_tests(void)
 {
@@ -58,26 +46,10 @@ void	ft_isvalid_tests(void)
 	ft_isvalid_test("5.000005s");
 	ft_isvalid_test("-+-+-5s");
 	ft_isvalid_test("-5#s");
+	ft_isvalid_test(".s");
+	ft_isvalid_test(".-s");
+	ft_isvalid_test(".-5s");
 	printf("\n\n");
-}
-
-void	ft_log_spec(t_spec *spec)
-{
-	printf("alternative_form: %d\n", spec->alternative_form);
-	printf("    zero_padding: %d\n", spec->zero_padding);
-	printf("      left_align: %d\n", spec->left_align);
-	printf("   leading_blank: %d\n", spec->leading_blank);
-	printf("    leading_sign: %d\n", spec->leading_sign);
-	printf("     field_width: %d\n", spec->field_width);
-	printf("       precision: %d\n", spec->precision);
-	printf("       specifier: %c\n", spec->specifier);
-}
-
-void	ft_parse_test(const char *specification, t_spec *spec)
-{
-	ft_print_test_desc(specification);
-	ft_parse(specification, spec);
-	ft_log_spec(spec);
 }
 
 void	ft_parse_tests(void)
@@ -111,6 +83,42 @@ void	ft_parse_tests(void)
 	ft_parse_test("-05.10s", spec);
 	ft_parse_test("5.000005s", spec);
 	ft_parse_test("-+-+-5s", spec);
+	ft_parse_test(".s", spec);
 	printf("\n\n");
 	free(spec);
+}
+
+static void	ft_isvalid_test(const char *specification)
+{
+	char *complete_spec;
+
+	complete_spec = ft_strjoin("%", specification);
+	ft_print_test_desc(specification);
+	printf(complete_spec, "Valid");
+	printf(" %s// printf%s\n", GRAY, COLOR_RESET);
+	if (ft_isvalid(specification))
+		printf("Valid");
+	else
+		printf("Not valid");
+	printf(" %s// isvalid%s\n", GRAY, COLOR_RESET);
+	free(complete_spec);
+}
+
+static void	ft_parse_test(const char *specification, t_spec *spec)
+{
+	ft_print_test_desc(specification);
+	ft_parse(specification, spec);
+	ft_log_spec(spec);
+}
+
+static void	ft_log_spec(t_spec *spec)
+{
+	printf("alternative_form: %d\n", spec->alternative_form);
+	printf("    zero_padding: %d\n", spec->zero_padding);
+	printf("      left_align: %d\n", spec->left_align);
+	printf("   leading_blank: %d\n", spec->leading_blank);
+	printf("    leading_sign: %d\n", spec->leading_sign);
+	printf("     field_width: %d\n", spec->field_width);
+	printf("       precision: %d\n", spec->precision);
+	printf("       specifier: %c\n", spec->specifier);
 }
